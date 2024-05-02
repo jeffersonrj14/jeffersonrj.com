c7d5abab3a02d3721d71e7fbbf2fb068553ea680
Jefferson <stupidjeff430@gmail.com>
2024-05-02 18:13:34 +0700

    feat: Add next-seo and update meta tags

--- src/components/PagesMetaHead.jsx
+++ src/components/PagesMetaHead.jsx
@@ -1,15 +1,34 @@
 import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 
 function PagesMetaHead({ title, keywords, description }) {
   return (
-    <Head>
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta name='keywords' content={keywords} />
-      <meta name='description' content={description} />
-      <meta charSet='utf-8' />
-      <link rel='icon' href='/author2.png' />
-      <title>{title}</title>
-    </Head>
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        keywords={keywords}
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: 'https://jeffersonrj.com',
+          siteName: 'Portfolio'
+        }}
+        twitter={{
+          handle: '@jeffersonrj14',
+          site: '@jeffersonrj14',
+          cardType: 'summary_large_image'
+        }}
+      />
+
+      {/*next/head for other customizations */}
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='keywords' content={keywords} />
+        <meta charSet='utf-8' />
+        <link rel='icon' href='/author2.png' />
+      </Head>
+    </>
   )
 }
 
