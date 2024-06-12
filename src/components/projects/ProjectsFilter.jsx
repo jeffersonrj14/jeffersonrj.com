39d052a267110a5c55b0cd03899fdf5e28de6b27
Jefferson <stupidjeff430@gmail.com>
2024-06-12 21:38:36 +0700

    refactor: the project search and filter are working, time to remove the old code (Remove commented-out code)

--- src/components/projects/ProjectsFilter.jsx
+++ src/components/projects/ProjectsFilter.jsx
@@ -30,38 +30,3 @@ function ProjectsFilter({ setSelectProject }) {
 }
 
 export default ProjectsFilter
-
-// const selectOptions = ['Portfolio', 'Project']
-
-// function ProjectsFilter({ setSelectProject }) {
-//   return (
-//     <select
-//       onChange={(e) => {
-//         setSelectProject(e.target.value)
-//       }}
-//       className='
-//                 px-4
-//                 sm:px-6
-//                 py-2
-//                 rounded-lg
-//                 text-sm
-//                 sm:text-md
-//                 font-medium
-//                 bg-jefferson-dark
-//                 text-jefferson-light
-//             '
-//     >
-//       <option value={setSelectProject} className='text-sm sm:text-md'>
-//         All Projects
-//       </option>
-
-//       {selectOptions.map((option) => (
-//         <option className='text-normal sm:text-md' key={option}>
-//           {option}
-//         </option>
-//       ))}
-//     </select>
-//   )
-// }
-
-// export default ProjectsFilter
